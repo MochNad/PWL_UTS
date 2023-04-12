e84f8a1634840fda0edbd7c07e979d021a93864b
fikisug <sugandafiki0@gmail.com>
2023-04-12 11:24:28 +0700

    fixSidebar - fikisug

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,56 +1,60 @@
 <aside class="main-sidebar sidebar-light-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="#" class="brand-link bg-primary">
-      <img src="https://i.pinimg.com/736x/c1/e2/81/c1e2818612a947ac94ef7b6aff3f8ece.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">PWL_UTS</span>
-    </a>
+  <!-- Brand Logo -->
+  <a href="#" class="brand-link bg-primary">
+    <img src="https://i.pinimg.com/736x/c1/e2/81/c1e2818612a947ac94ef7b6aff3f8ece.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">PWL_UTS</span>
+  </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
-        </div>
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="fas fa-utensils pr-3"></i>
-              <p>
-                Menu
-                <i class="fas fa-angle-left right"></i>
-                <span class="badge badge-danger right">New</span>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="{{ url('/food') }}" class="nav-link">
-                  <i class="fas fa-hamburger pr-3"></i>
-                  <p>Food</p>
-                  <span class="badge badge-info right">{{ $countFood }}</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{ url('/drink') }}" class="nav-link">
-                  <i class="fas fa-cocktail pr-3"></i>
-                  <p>Drink</p>
-                  <span class="badge badge-info right">{{ $countDrink }}</span>
-                </a>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
+      <div class="info">
+        <a href="#" class="d-block">{{ Auth::user()->name }}</a>
+      </div>
     </div>
-    <!-- /.sidebar -->
-  </aside>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="fas fa-utensils pr-3"></i>
+            <p>
+              Menu
+              <i class="fas fa-angle-left right"></i>
+              <span class="badge badge-danger right">New</span>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="{{ url('/food') }}" class="nav-link">
+                <i class="fas fa-hamburger pr-3"></i>
+                <p>Food
+                  <span class="badge badge-info right">{{ $countFood }}</span>
+                </p>
+                
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/drink') }}" class="nav-link">
+                <i class="fas fa-cocktail pr-3"></i>
+                <p>Drink
+                  <span class="badge badge-info right">{{ $countDrink }}</span>
+                </p>
+                
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
